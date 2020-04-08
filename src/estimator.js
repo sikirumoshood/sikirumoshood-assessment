8a426f632d01ea27d9748a888585c7a9034e718f
Moshood Sikiru <sikirumoshood@gmail.com>
2020-04-08 20:35:19 +0100

    Add time to elapse

--- src/estimator.js
+++ src/estimator.js
@@ -3,17 +3,17 @@ import { estimateNumberOfInfectedPeople, estimateInfectionsByRequestedTime } fro
 const covid19ImpactEstimator = (data) => {
   const inputData = data;
   const { reportedCases, periodType, timeToElapse } = data;
-  const mildCurrentlyInfected = estimateNumberOfInfectedPeople(reportedCases, timeToElapse);
-  const severeCurrentlyInfected = estimateNumberOfInfectedPeople(reportedCases, timeToElapse);
+  const mildCurrentlyInfected = estimateNumberOfInfectedPeople(reportedCases, 10);
+  const severeCurrentlyInfected = estimateNumberOfInfectedPeople(reportedCases, 50);
   const infectionsByRequestedTimeForMildCase = estimateInfectionsByRequestedTime(
     mildCurrentlyInfected,
-    28,
+    timeToElapse,
     3,
     periodType
   );
   const infectionsByRequestedTimeForSevereCase = estimateInfectionsByRequestedTime(
     severeCurrentlyInfected,
-    28,
+    timeToElapse,
     3,
     periodType
   );
