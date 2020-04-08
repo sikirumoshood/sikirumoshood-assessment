0a9b318c4c2bdaf2f0f918ff8761b5509d00e7c1
Moshood Sikiru <sikirumoshood@gmail.com>
2020-04-08 20:20:28 +0100

    Make adjustment to period

--- src/estimator.js
+++ src/estimator.js
@@ -2,11 +2,21 @@ import { estimateNumberOfInfectedPeople, estimateInfectionsByRequestedTime } fro
 
 const covid19ImpactEstimator = (data) => {
   const inputData = data;
-  const { reportedCases } = data;
+  const { reportedCases, periodType } = data;
   const mildCurrentlyInfected = estimateNumberOfInfectedPeople(reportedCases, 10);
   const severeCurrentlyInfected = estimateNumberOfInfectedPeople(reportedCases, 50);
-  const infectionsByRequestedTimeForMildCase = estimateInfectionsByRequestedTime(mildCurrentlyInfected, 28, 3, 'days');
-  const infectionsByRequestedTimeForSevereCase = estimateInfectionsByRequestedTime(severeCurrentlyInfected, 28, 3, 'days');
+  const infectionsByRequestedTimeForMildCase = estimateInfectionsByRequestedTime(
+    mildCurrentlyInfected,
+    28,
+    3,
+    periodType
+  );
+  const infectionsByRequestedTimeForSevereCase = estimateInfectionsByRequestedTime(
+    severeCurrentlyInfected,
+    28,
+    3,
+    periodType
+  );
 
   return {
     data: inputData,
