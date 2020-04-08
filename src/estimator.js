aff73ab7c0990d874970a81f1b7c903a90e51774
Moshood Sikiru <sikirumoshood@gmail.com>
2020-04-08 18:59:35 +0100

    Stage-one-solution

--- src/estimator.js
+++ src/estimator.js
@@ -1,3 +1,26 @@
-const covid19ImpactEstimator = (data) => data;
+import * as E from './utils/estimate';
+
+const { estimateNumberOfInfectedPeople, estimateInfectionsByRequestedTime } = E;
+
+const covid19ImpactEstimator = (data) => {
+  const inputData = data;
+  const { reportedCases } = data;
+  const mildCurrentlyInfected = estimateNumberOfInfectedPeople(reportedCases, 10);
+  const severeCurrentlyInfected = estimateNumberOfInfectedPeople(reportedCases, 50);
+  const infectionsByRequestedTimeForMildCase = estimateInfectionsByRequestedTime(mildCurrentlyInfected, 28, 3, 'days');
+  const infectionsByRequestedTimeForSevereCase = estimateInfectionsByRequestedTime(severeCurrentlyInfected, 28, 3, 'days');
+
+  return {
+    data: inputData,
+    impact: {
+      currentlyInfected: mildCurrentlyInfected,
+      infectionsByRequestedTime: infectionsByRequestedTimeForMildCase
+    },
+    severeImpact: {
+      currentlyInfected: severeCurrentlyInfected,
+      infectionsByRequestedTime: infectionsByRequestedTimeForSevereCase
+    }
+  };
+};
 
 export default covid19ImpactEstimator;
---
+++ src/utils/estimate.js
@@ -0,0 +1,44 @@
+/*
+    @Description: This method estimates the number of currently infected
+    people.
+    @params reportedCases { Number }
+    @params factor { Number }
+    @returns currentlyInfected { Number }
+
+
+*/
+
+const estimateNumberOfInfectedPeople = (reportedCases, factor) => {
+  const currentlyInfected = parseFloat(reportedCases) * parseFloat(factor);
+  return currentlyInfected;
+};
+
+
+/*
+    @description: This method return the estimated number of infections for a requested
+         period of time.
+    @params currentlyInfect { Number }
+    @params duration { Number }
+    @params factor { Number }
+    @params type { String }
+    @returns estimatedInfections { Number }
+
+*/
+
+const estimateInfectionsByRequestedTime = (currentlyInfected, duration, frequency, type) => {
+  let days;
+  switch (type) {
+    case 'weekly': days = parseFloat(duration) * 7;
+      break;
+    case 'monthly': days = parseFloat(duration) * 30;
+      break;
+    default: days = parseFloat(duration);
+  }
+  const currentlyInfectedNumber = parseFloat(currentlyInfected);
+  const frequencyNumber = parseFloat(frequency);
+  const estimatedInfections = currentlyInfectedNumber * Math.floor(days / frequencyNumber);
+
+  return estimatedInfections;
+};
+
+export default { estimateNumberOfInfectedPeople, estimateInfectionsByRequestedTime };
