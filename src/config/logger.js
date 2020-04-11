3e8c69894a3f4350d358e1be5518dcc0b720ed54
Moshood Sikiru <sikirumoshood@gmail.com>
2020-04-11 09:36:10 +0100

    Fix log issue

--- src/config/logger.js
+++ src/config/logger.js
@@ -13,12 +13,20 @@ class Logger {
     const TIME_ELAPSED = duration.as('milliseconds');
 
     fs.appendFile(config.logDirectory, `${METHOD}\t\t${URL}\t\t${STATUS}\t\t${TIME_ELAPSED} ms\n`, (err) => {
-      if (err) throw new Error(err.message);
+      if (err) {
+        throw new Error(err.message);
+      }
     });
   }
 
   static readLogs() {
-    return fs.readFileSync(config.logDirectory, 'utf-8');
+    const logLocationExists = fs.existsSync(config.logDirectory);
+    if (logLocationExists) {
+      return fs.readFileSync(config.logDirectory, 'utf-8');
+    }
+    // Create one
+    fs.appendFileSync(config.logDirectory, '');
+    return '';
   }
 }
 
